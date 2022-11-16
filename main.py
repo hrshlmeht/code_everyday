faa13171f550102ae890bc396c9dc788d9362721
hrshlmeht <harshalmehta1998@gmail.com>
2022-11-16 17:37:31 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# print('Enter the size of the array')
+# arr_size = int(input())
+# nums =[]
+# for j in range(arr_size):
+#     print('Enter the element')
+#     nums.append(input())
+#
+#
+# print (nums)
+# robber_1 = 0
+# robber_2 = 0
+# for i in nums:
+#     temp = max(robber_1 + int(i) , robber_2)
+#     robber_1 = robber_2
+#     robber_2 = temp
+# # return robber_2
+# print (robber_2)
+
+
+
