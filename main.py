0185bdd89ab6f6848ae821e3e96c05465730b8c2
hrshlmeht <harshalmehta1998@gmail.com>
2022-11-21 00:38:23 -0500

    second code

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
-# print('Enter the size of the array')
-# arr_size = int(input())
-# nums =[]
-# for j in range(arr_size):
-#     print('Enter the element')
-#     nums.append(input())
-#
-#
-# print (nums)
-# robber_1 = 0
-# robber_2 = 0
-# for i in nums:
-#     temp = max(robber_1 + int(i) , robber_2)
-#     robber_1 = robber_2
-#     robber_2 = temp
-# # return robber_2
-# print (robber_2)
+class Person:
+  def __init__(self, name=None, age=None):
+    self.name = name
+    self.age = age
 
-# print('Enter the string to check the palindrome')
-# x = input()
-#
-# reverse_x = x[::-1]
-# print(reverse_x)
-# if reverse_x == x:
-#     print('The entered string is a palindrome')
-# else:
-#     print('The entered string is not a paindrome')
-#
+  def __str__(self):
+    return f"Name:{self.name} Age:({self.age})"
 
+p1 = Person(age=36)
 
-def calculator():
-    print('Enter the numbers for addition')
-    num1 = int(input())
-    num2 = int(input())
-    sum  = num1 + num2
-    print('The sum is = ',sum)
-
-calculator()
-
+print(p1.name)
+print(p1)
