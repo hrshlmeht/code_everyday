be3b729ece31bcd118a87abc74fc607fa3b7db03
hrshlmeht <harshalmehta1998@gmail.com>
2022-11-22 16:30:16 -0500

    zang zing zung

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-class Person:
-  def __init__(self, name=None, age=None):
-    self.name = name
-    self.age = age
+# class Person:
+#   def __init__(self, name=None, age=None):
+#     self.name = name
+#     self.age = age
+#
+#   def __str__(self):
+#     return f"Name:{self.name} Age:({self.age})"
+#
+# p1 = Person(age=36)
+#
+# print(p1.name)
+# print(p1)
+#
 
-  def __str__(self):
-    return f"Name:{self.name} Age:({self.age})"
 
-p1 = Person(age=36)
+class Dog:
+  def  bark (self ):
+        print("The dogo is barking")
 
-print(p1.name)
-print(p1)
+  def yolo (self):
+      print ("Munni badnam")
+
+
+d = Dog()
+d.bark()
+d.yolo()
+
+
+
