bc8ffb39003ec5edef52de9f4c19b6f56fa5a9e2
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-04 18:43:16 -0500

    hackerrank problem

--- main.py
+++ main.py
@@ -467,3 +467,15 @@ import sys
 #
 #     fptr.close()
 
+#leetcode remove element
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = 0
+#         for x in nums:
+#             if x != val:
+#                 nums[i] = x
+#                 i += 1
+#         return i
+#
+#
+
