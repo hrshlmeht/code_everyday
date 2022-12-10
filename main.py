543cc3e38402acebf462493834a134d094eb3d7f
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-10 00:24:39 -0500

    currency convertor done

--- main.py
+++ main.py
@@ -382,7 +382,7 @@
 
 ###get the data from the link - https://data.nba.net/prod/v1/today.json (open API)
 
-from requests import get
+# from requests import get
 # from pprint import PrettyPrinter
 #
 # BASE_URL = "https://data.nba.net"
@@ -626,32 +626,103 @@ import sys
 # ON
 # p1.email = p2.email
 # and p1.id < p2.id
-from pprint import PrettyPrinter
+# from pprint import PrettyPrinter
+#
+# BASE_URL = "https://free.currconv.com/"
+# API_KEY = '7dc769f16b1b0ef349b0'
+#
+# printer = PrettyPrinter()
+#
+#
+# def get_currencies():
+#     endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+#     url = BASE_URL + endpoint
+#     data = get(url).json()['results']
+#     data = list(data.items())
+#     data.sort()
+#
+#     return data
+#
+#
+# def print_currencies(currencies):
+#     for name, currency in currencies:
+#         name = currency['currencyName']
+#         _id = currency['id']
+#
+#         symbol = currency.get('currencySymbol', '')
+#         print(f"{_id} - {name} - {symbol}")
+#
+#
+# def exchange_rate(curr1 , curr2):
+#     endpoint = f"api/v7/convert?q={curr1}_{curr2}&compact=ultra&apiKey={API_KEY}"
+#     url = BASE_URL + endpoint
+#     response = get(url)
+#     data = response.json()
+#
+#     if len(data) == 0:
+#         print('Invalid cureencies')
+#         return
+#
+#     rate = list(data.values())[0]
+#     print(f'{curr1} -> {curr2} = {data}')
+#
+#     return rate
+#
+# def convert(curr1 , curr2 , amount):
+#     rate = exchange_rate(curr1 , curr2)
+#     if rate is None:
+#         return
+#
+#     try:
+#         amount = float(amount)
+#     except:
+#         print('Invalid amount')
+#         return
+#
+#     converted_amount = rate * amount
+#     print(f'{amount} {curr1} is equal to {converted_amount}{curr2}')
+#     return converted_amount
+#
+# # data = get_currencies()
+# # print_currencies(data)
+# rate=exchange_rate("CAD", "USD")
+# print(rate)
+#
+#
+# def main():
+#     currencies = get_currencies()
+#     print('Hello apun ne currency convert karega')
+#     print('List - lists the different currencies')
+#     print('Convert - convert from one currency to another')
+#     print('Rate - get the exchange amount of the two currencies')
+#     print()
+#
+#     while True:
+#         command = input('Enter a command (q to quit: ').lower()
+#
+#         if command == 'q':
+#             break
+#         elif command == 'list':
+#             print_currencies(currencies)
+#         elif command == 'convert':
+#             curr1 = input('Enter a base currency : ').upper()
+#             amount = input(f"Enter an amount in {curr1} : ")
+#             curr2 = input('Enter a currency to convert to :').upper()
+#             convert(curr1 , curr2 , amount)
+#         elif command == 'rate':
+#             curr1 = input('Enter the base currency: ').upper()
+#             curr2 = input('Enter a currency you want to convert to :').upper()
+#             exchange_rate(curr1 , curr2)
+#         else:
+#             print("Unrecognized command")
+#
+#
+#
+# main()
 
-BASE_URL = "https://free.currconv.com/"
-API_KEY = '7dc769f16b1b0ef349b0'
-
-printer = PrettyPrinter()
 
 
-def get_currencies():
-    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
-    url = BASE_URL + endpoint
-    data = get(url).json()['results']
-    data = list(data.items())
-    data.sort()
-
-    return data
 
 
-def print_currencies(currencies):
-    for name, currency in currencies:
-        name = currency['currencyName']
-        _id = currency['id']
-
-        symbol = currency.get('currencySymbol', '')
-        print(f"{_id} - {name} - {symbol}")
 
 
-data = get_currencies()
-print_currencies(data)
