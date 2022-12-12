080179761c1302f734c80ddbe4ff217f503cc5ec
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-11 20:47:06 -0500

    learning linked list

--- main.py
+++ main.py
@@ -745,20 +745,33 @@ import sys
 #     print (' is is tatti')
 #
 #
-# class Node:
-#     def __init__(self , data=None , next = None):
-#         self.data = data
-#         self.next = next
-#
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-#
-#     def insert_at_head(self, data):
-#         node = Node(data , self.head)
-#         self.head = node
+class Node:
+    def __init__(self , data=None , next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_head(self, data):
+        node = Node(data , self.head)
+        self.head = node
 
 
+    def print(self):
+        itr = self.head
+        llstr = ' '
+        while itr:
+            llstr += str(itr.data) + '--->'
+            itr = itr.next
+        print(llstr)
 
+
+if __name__ == "__main__":
+    root = LinkedList()
+    root.insert_at_head(5)
+    root.insert_at_head(12)
+    root.print()
 
 
