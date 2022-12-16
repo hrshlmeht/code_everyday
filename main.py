59d021fc0f86aa630ddc6170c524aa459e9a8e86
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-15 22:29:03 -0500

    leetcode2 done

--- main.py
+++ main.py
@@ -919,4 +919,42 @@ import sys
 #         return True
 # else:
 #     return False
+################################### implementing queue with stacks (Real game)
+# class MyQueue(object):
+#
+#     def __init__(self):
+#         self.s1 = []
+#         self.s2 = []
+#
+#     def push(self, x):
+#         while self.s1:
+#             self.s2.append(self.s1.pop())
+#         self.s1.append(x)
+#         while self.s2:
+#             self.s1.append(self.s2.pop())
+#
+#     def pop(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.s1.pop()
+#
+#     def peek(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.s1[-1]
+#
+#     def empty(self):
+#         """
+#         :rtype: bool
+#         """
+#         return not self.s1
 
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
