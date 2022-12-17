0a23b227ea2ea5240f88597606d4839b0a6aa5f9
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-16 21:23:05 -0500

    leetcode1 done

--- main.py
+++ main.py
@@ -958,3 +958,13 @@ import sys
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+### replace elements with greatest on the right side
+# class Solution(object):
+#     def replaceElements(self, arr):
+#         right_max = -1
+#         for i in range(len(arr)-1, -1 ,-1 ):
+#            new_max = max(arr[i] , right_max)
+#            arr[i] = right_max
+#            right_max= new_max
+#         return arr
