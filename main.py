e001719ff3449b7cea6600c30fb939d20733f466
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-16 22:05:45 -0500

    leetcode2 done

--- main.py
+++ main.py
@@ -968,3 +968,23 @@ import sys
 #            arr[i] = right_max
 #            right_max= new_max
 #         return arr
+
+
+##### Leetcode problem - Evaluate reverse polish operation
+# class Solution(object):
+#     def evalRPN(self, tokens):
+#         stack = []
+#         for char in tokens:
+#             if char == '+':
+#                 stack.append(stack.pop() + stack.pop())
+#             elif char == '-':
+#                  a , b = stack.pop() , stack.pop()
+#                  stack.append(b-a)
+#             elif char == '*':
+#                  stack.append(stack.pop() * stack.pop())
+#             elif char == '/':
+#                  a , b = stack.pop() , stack.pop()
+#                  stack.append(int(int(b)/int(a)))
+#             else:
+#                 stack.append(int(char))
+#         return stack[0]
