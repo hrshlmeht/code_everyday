095b40325f6d0aa4d2c95e6b19d56720b803de00
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-20 20:44:18 -0500

    leetcode3 done

--- main.py
+++ main.py
@@ -1080,4 +1080,19 @@ import sys
 #
 #         return result.values()
 
-
+############################ Pascals Triangle####################
+# class Solution(object):
+#     def generate(self, numRows):
+#         """
+#         :type numRows: int
+#         :rtype: List[List[int]]
+#         """
+#         res = [[1]]
+#
+#         for i in range(numRows - 1 ):
+#             temp = [0] + res[-1] + [0]
+#             row = []
+#             for j in range(len(res[-1])+ 1):
+#                 row.append(temp[j]+ temp[j +1])
+#             res.append(row)
+#         return res
