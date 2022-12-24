8b553eace201a7141e91b66cfa09a1c1ac26fd8d
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-23 20:17:52 -0500

    leetcode1 done

--- main.py
+++ main.py
@@ -1212,3 +1212,26 @@ import sys
 #
 #         return res
 #
+#################next greater element#################
+# class Solution(object):
+#     def nextGreaterElement(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: List[int]
+#         """
+#
+#         nums1_Index = {n: i for i, n in enumerate(nums1)}
+#         res = [-1] * len(nums1)
+#
+#         for i in range(len(nums2)):
+#             if nums2[i] not in nums1_Index:
+#                 continue
+#             for j in range(i + 1, len(nums2)):
+#                 if nums2[j] > nums2[i]:
+#                     index = nums1_Index[nums2[i]]
+#                     res[index] = nums2[j]
+#                     break
+#
+#         return res
+
