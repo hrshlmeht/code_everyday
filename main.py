8f61d8920753c44d6ce817b3aa6a8acaa5ece483
hrshlmeht <harshalmehta1998@gmail.com>
2022-12-27 12:48:47 -0500

    leetcode1 done

--- main.py
+++ main.py
@@ -1314,3 +1314,28 @@ import sys
 #             WordsToChar[w] = c
 #
 #         return True
+
+
+########################################Top K frequent elements####################################
+# class Solution(object):
+#     def topKFrequent(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: List[int]
+#         """
+#         count = {}
+#         freq = [[] for i in range(len(nums) + 1)]
+#
+#         for n in nums:
+#             count[n] = 1 + count.get(n, 0)
+#
+#         for n, c in count.items():
+#             freq[c].append(n)
+#
+#         res = []
+#         for i in range(len(freq) - 1, 0, -1):
+#             for n in freq[i]:
+#                 res.append(n)
+#                 if len(res) == k:
+#                     return res
