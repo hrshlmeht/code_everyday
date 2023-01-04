22320d225cc59afba49a70528a704a35348deeaf
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-04 00:01:27 -0500

    pivot revised

--- main.py
+++ main.py
@@ -1504,3 +1504,22 @@ import sys
 #
 #         return -1
 #
+##############daily coding challenge leetcode####################
+# class Solution:
+#     def minimumRounds(self, tasks: List[int]) -> int:
+#         frequency = Counter(tasks)
+#         print(frequency)
+#         print (frequency.values())
+#         res = 0
+#         for freq in frequency.values():
+#             if freq == 1:
+#                 return -1
+#             elif (freq-2) % 3 == 0:
+#                 res += (freq-2) // 3 + 1
+#             elif (freq - 4) % 3 == 0:
+#                 res += (freq - 4) // 3 +2
+#             elif freq % 3 == 0:
+#                 res += freq // 3
+#             else:
+#                 res += freq // 2
+#         return res
