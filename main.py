392cc262538334ad6cdf1664f1a45964da68bc2f
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-05 21:19:37 -0500

    leetcode

--- main.py
+++ main.py
@@ -1578,4 +1578,19 @@ import sys
 #                 ending = balloon[1]
 #
 #         return count
-##########################
+##########################leetcode daily problem###############################
+# class Solution:
+#     def maxIceCream(self, costs: List[int], coins: int) -> int:
+#         # Store ice cream costs in increasing order.
+#         costs.sort()
+#         n, icecream = len(costs), 0
+#
+#         # Pick ice creams till we can.
+#         while icecream < n and costs[icecream] <= coins:
+#             # We can buy this icecream, reduce the cost from the coins.
+#             coins -= costs[icecream]
+#             icecream += 1
+#
+#         return icecream
+
+
