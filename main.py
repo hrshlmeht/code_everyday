bb0ff3179923df7656051ab9053350eaac30c4b6
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-11 23:45:21 -0500

    leetcode1

--- main.py
+++ main.py
@@ -1742,7 +1742,15 @@ import sys
 #             print(i , j )
 
 
-
-
-
-
+#################sorting the colours##################
+# class Solution:
+#     def sortColors(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] > nums[j]:
+#                     nums[i], nums[j] = nums[j], nums[i]
+#         print(nums)
