060d6337f5a9774adcb373055d53806e60b8c81d
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-16 19:54:57 -0500

    leetcode 2

--- main.py
+++ main.py
@@ -2404,3 +2404,47 @@ import sys
 #             data += i
 #
 #         return (int(data))
+####################workin solution##############(need to revise)
+# class Solution(object):
+#     def myAtoi(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         s = s.lstrip()
+#         print(s)
+#
+#         if not s:
+#             return 0
+#
+#         if len(s) == 1 and not s[0].isdigit():
+#             return 0
+#
+#         i = 0
+#
+#         sign = 1
+#
+#         if s[i] == "+":
+#             i += 1
+#         if s[i] == "-":
+#             sign = -1
+#             i += 1
+#
+#         if i == 2:
+#             return 0
+#
+#         parsed = 0
+#
+#         while i < len(s):
+#             if not (s[i]).isdigit():
+#                 break
+#             else:
+#                 parsed = parsed * 10 + int(s[i])
+#             i += 1
+#         parsed = sign * parsed
+#         if parsed > 2 ** 31 - 1:
+#             return 2 ** 31 - 1
+#         elif parsed <= -2 ** 31:
+#             return -2 ** 31
+#         else:
+#             return parsed
