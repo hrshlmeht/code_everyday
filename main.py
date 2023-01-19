6b9248579ba13947d968b6372eee399b0963c958
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-18 19:33:49 -0500

    leetcode1 done

--- main.py
+++ main.py
@@ -2514,3 +2514,21 @@ import sys
 #
 #         return result
 
+#########################working solution on leetcode#####################
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#
+#         res = 0
+#
+#         left = 0
+#         right = len(height) - 1
+#         while left < right:
+#             area = (right - left) * min(height[left], height[right])
+#             res = max(area, res)
+#
+#             if height[right] > height[left]:
+#                 left += 1
+#             else:
+#                 right -= 1
+#
+#         return res
