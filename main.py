ba07f0442e80da2cb09a701dfe25285e3357958e
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-23 02:07:14 -0500

    leetcode

--- main.py
+++ main.py
@@ -2591,3 +2591,24 @@ import sys
 #         candidates.append(num)
 #         print(candidates)
 
+
+############################insert interval########
+# class Solution:
+#     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+#         result = []
+#         for i in intervals:
+#             if i[1] < newInterval[0]:
+#                 result.append(i)
+#
+#             elif i[0] > newInterval[1]:
+#                 result.append(newInterval)
+#                 newInterval = i
+#
+#             elif i[1] >= newInterval[0] or i[0] <= newInterval[1]:
+#                 newInterval[0] = min(i[0], newInterval[0])
+#                 newInterval[1] = max(newInterval[1], i[1])
+#
+#         result.append(newInterval);
+#         return result
+
+
