f4bbc4d97ef4ffb9edccad82073a5a6534104222
hrshlmeht <harshalmehta1998@gmail.com>
2023-01-29 13:30:12 -0500

    leetcode

--- main.py
+++ main.py
@@ -2770,3 +2770,24 @@ import sys
 #             return (self.arr[right] - self.arr[left - 1])
 #         else:  # in case left is 0,the sum in the 'right' position will give the total sum required
 #             return (self.arr[right])
+#################range sum querry with inbuilt functions###############
+# class Codec:
+#     def __init__(self):
+#         self.encodeMap = {}
+#         self.decodeMap = {}
+#         self.base = "http://tinyurl.com/"
+#
+#     def encode(self, longUrl: str) -> str:
+#         """Encodes a URL to a shortened URL.
+#         """
+#         if longUrl not in self.encodeMap:
+#             shortUrl = self.base + str(len(self.encodeMap) + 1)
+#             self.encodeMap[longUrl] = shortUrl
+#             self.decodeMap[shortUrl] = longUrl
+#         return self.encodeMap[longUrl]
+#
+#     def decode(self, shortUrl: str) -> str:
+#         """Decodes a shortened URL to its original URL.
+#         """
+#         return self.decodeMap[shortUrl]
+
