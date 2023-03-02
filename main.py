99852da72c3098c4a07e0bc307c18ee484443343
hrshlmeht <harshalmehta1998@gmail.com>
2023-03-01 23:45:44 -0500

    subarray sum = k

--- main.py
+++ main.py
@@ -3020,4 +3020,18 @@ import sys
 #         print(ans)
 #         return len(ans)
 
+###################leetcode solution working using Hashamps##############
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         count = 0
+#         sum = 0
+#         dic = {}
+#         dic[0] = 1
+#         for i in range(len(nums)):
+#             sum += nums[i]
+#             if sum-k in dic:
+#                 count += dic[sum-k]
+#             dic[sum] = dic.get(sum, 0)+1
+#         return count
+#
 
