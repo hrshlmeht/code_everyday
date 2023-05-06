b740c7b78b8dc35b749e0f93bc35f51db3c8114d
hrshlmeht <harshalmehta1998@gmail.com>
2023-05-06 15:42:03 -0400

    Leetcode problem 1 done

--- main.py
+++ main.py
@@ -3101,3 +3101,22 @@ import sys
 #         #             ans.append(nums2[i])
 #
 #         return ans
+
+# class Solution:
+#     def numIdenticalPairs(self, nums: List[int]) -> int:
+#         my_count = 0
+#         my_dict = {}
+#
+#         for n in nums:
+#             if n in my_dict:
+#                 # Increase count by the number of previous instances
+#                 my_count += my_dict[n]
+#
+#                 # Increase the count of previous observation
+#                 my_dict[n] +=1
+#             else:
+#                 # Store newly encountered number along with its count
+#                 my_dict[n] = 1
+#
+#         return my_count
+
