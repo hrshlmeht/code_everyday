666c3ca8b9d025438c1b48067ab1991cfc38ccb3
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-10 16:37:56 -0400

    Leetcode code for  the day

--- main.py
+++ main.py
@@ -3402,6 +3402,18 @@ import sys
 #             last += avg
 
 #         return out
+# class Solution:
+#     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+#         n = len(grid)
+#         matrix = [[1] * (n - 2) for i in range(n - 2)]
+#         for i in range(1, n - 1):
+#             for j in range(1, n - 1):
+#                 matrix[i - 1][j - 1] = max(grid[i - 1][j - 1], grid[i - 1][j], grid[i - 1][j + 1],
+#                                            grid[i][j - 1], grid[i][j], grid[i][j + 1],
+#                                            grid[i + 1][j - 1], grid[i + 1][j], grid[i + 1][j + 1])
+#         return matrix
+#
+#
 
 
 
