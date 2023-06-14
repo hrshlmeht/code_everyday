adcb0660adfee10dbb9301e3dce3cf3f4d70e68b
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-14 19:07:44 -0400

    Leetcode code for  the day

--- main.py
+++ main.py
@@ -3594,4 +3594,11 @@ import sys
 #
 # #         print (ans)
 #
-
+# class Solution:
+#     def sumOfMultiples(self, n: int) -> int:
+#         ans = []
+#         for i in range(1, n + 1):
+#             if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+#                 ans.append(i)
+#
+#         return sum(ans)
