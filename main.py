1b9989d33aff5a83c481fc16db1a74aebf0530f8
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-15 22:31:49 -0400

    NeetCode Problem solved

--- main.py
+++ main.py
@@ -14,3 +14,29 @@
 #             return True
 #         else:
 #             return False
+
+#####TWO POINTER - Two sum 2 (input array is sorted)
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#
+#         l, r = 0, len(numbers) - 1
+#         while l < r:
+#             s = numbers[l] + numbers[r]
+#             if s == target:
+#                 return [l + 1, r + 1]
+#             elif s < target:
+#                 l += 1
+#             else:
+#                 r -= 1
+#
+# #         ans = []
+# #         for i in numbers:
+# #             j = target- i
+# #             if j in numbers:
+# #                 k = numbers.index(j)
+# #                 j = i
+# #                 ans.append(k+1)
+#
+#
+# #         print(sorted(ans))
+#
