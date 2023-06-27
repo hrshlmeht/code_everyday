6dc9931289cc1e3c024e2ae0bf79e73825130b50
hrshlmeht <harshalmehta1998@gmail.com>
2023-06-26 22:28:13 -0400

    Leetcode Problem solved

--- main.py
+++ main.py
@@ -147,3 +147,17 @@
 #                 result += maxRight - height[right]
 #
 #         return result
+
+
+
+#sum of all off length subarrays
+# class Solution:
+#     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+#         n = len(arr)
+#         answer = 0
+#         for left in range(n):
+#             current_sum = 0
+#             for right in range(left, n):
+#                 current_sum += arr[right]
+#                 answer += current_sum if (right - left + 1) % 2 == 1  else 0
+#         return answer
