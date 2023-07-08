e2f2675eb49edeee2b57a3a9a8019fc8fdd2fd8b
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-08 00:12:00 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -410,3 +410,15 @@
 #
 # #         print (area)
 # #         return area
+################ finding the maximum perimeter of triangle from given lengths in an array########### the tesla problem
+#
+# class Solution:
+#     def largestPerimeter(self, nums: List[int]) -> int:
+#         nums.sort(reverse=True)
+#
+#         for i in range(3, len(nums) + 1):
+#
+#             if (nums[i - 3] < nums[i - 2] + nums[i - 1]):
+#                 return sum(nums[i - 3:i])
+#
+#         return 0
