9353e0325acfe0a89d97e27eb0a9af89e7004153
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-12 23:24:40 -0400

    Neetcode done

--- main.py
+++ main.py
@@ -463,3 +463,25 @@
 #         return sum(ans)
 
 
+######min stack neetcode######
+# class MinStack:
+#     def __init__(self):
+#         self.stack = []
+#         self.minStack = []
+#
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+#         if self.minStack:
+#             self.minStack.append(min(val, self.minStack[-1]))
+#         else:
+#             self.minStack.append(val)
+#
+#     def pop(self) -> None:
+#         self.stack.pop()
+#         self.minStack.pop()
+#
+#     def top(self) -> int:
+#         return self.stack[-1]
+#
+#     def getMin(self) -> int:
+#         return self.minStack[-1]
