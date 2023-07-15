be747adf810bbf2cd8a3c534473002d55f7a2535
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-15 11:48:31 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -549,3 +549,20 @@
 #             return True
 #         else:
 #             return False
+
+
+#####finding the strings within a range with vowels#######
+
+# class Solution:
+#     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
+#         vowel = ['a', 'e', 'i', 'o', 'u']
+#         count = 0
+#         for i in range(left, right + 1):
+#             print(words[i][0])
+#             print(words[i][-1])
+#             if words[i][0] in vowel and words[i][-1] in vowel:
+#                 count += 1
+#                 print(count)
+#
+#         print(count)
+#         return count
