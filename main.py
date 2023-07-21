f79e09fe119abf4ed9b55b01e7f8559f27ce8756
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-20 23:50:33 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -762,3 +762,25 @@
 #
 # #         #return len(ans)
 #
+#######*************important problem - asked multiple times -- stack --************
+# class Solution:
+#     def evalRPN(self, tokens: List[str]) -> int:
+#         stack = []
+#
+#         for op in tokens:
+#             if op == '+':
+#                 stack.append(stack.pop() + stack.pop())
+#             elif op == '-':
+#                 a = stack.pop()
+#                 b = stack.pop()
+#                 stack.append(b - a)
+#             elif op == '*':
+#                 stack.append(stack.pop() * stack.pop())
+#             elif op == '/':
+#                 a = stack.pop()
+#                 b = stack.pop()
+#                 stack.append(int(b / a))
+#             else:
+#                 stack.append(int(op))
+#
+#         return stack[0]
