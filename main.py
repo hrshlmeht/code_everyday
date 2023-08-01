379b1aefeb623b587d2a8403db6833c5e684e181
hrshlmeht <harshalmehta1998@gmail.com>
2023-07-31 23:04:56 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1013,3 +1013,18 @@
 #             return True
 #
 #         return False
+
+#########number of string that appear a sa substring word###
+
+# class Solution:
+#     def numOfStrings(self, patterns: List[str], word: str) -> int:
+#
+#         count = 0
+#
+#         for i in patterns:
+#             if i in word:
+#                 count += 1
+#
+#         print(count)
+#
+#         return (count)
