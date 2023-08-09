d4a07e7c3485bbaad82d0e4ed761c1bd2a639551
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-08 20:40:43 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1260,3 +1260,19 @@
 #                 ans.append(ans_string)
 #
 #         print(ans)
+
+########sort array by parity#######
+# class Solution:
+#     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+#         even = []
+#         odd = []
+#         lst=[]
+#         for i in range(len(nums)):
+#             if nums[i]%2 == 0:
+#                 even.append(nums[i])
+#             else:
+#                 odd.append(nums[i])
+#         for i in range(len(even)):
+#             lst.append(even[i])
+#             lst.append(odd[i])
+#         return lst
