0942d519eb3b76c85028ecdc69847a85380d087c
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-14 23:56:41 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1373,3 +1373,17 @@
 #                     break
 #
 #         return dp[0]
+
+
+###########toeplitiz matrix##########
+# class Solution:
+#     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+#         for i in range(0, len(matrix) - 1):
+#
+#             for j in range(0, len(matrix[i]) - 1):
+#
+#                 if matrix[i][j] != matrix[i + 1][j + 1]:
+#                     return False
+#
+#         return True
+#
