75bdf906178b8c6459c97c1a7610e84b34ed83de
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-16 18:30:40 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1438,3 +1438,15 @@
 #         #         return False
 #
 #         # return True
+
+
+######checking the jewels and stones#######
+# class Solution:
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         count = 0
+#         for i in stones:
+#             if i in jewels:
+#                 count += 1
+#
+#         print(count)
+#         return count
