10b55d7456d0d03cd66552cd90ff67218903934f
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-18 22:11:59 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1475,3 +1475,9 @@
 # class Solution:
 #
 
+#########leetcode strings######
+# class Solution:
+#     def interpret(self, command: str) -> str:
+#         command = command.replace('()', 'o')
+#         command = command.replace('(al)', 'al')
+#         return command
