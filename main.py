69aa1a5d0a6c2b2e3fcaeadd1748db844d86b264
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-24 20:21:47 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1505,3 +1505,19 @@
 #             ans.append(lower)
 #
 #         return ans
+
+#######leetcode solution for = relative sort array##########
+# class Solution:
+#     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+#         count = [0] * 1001
+#
+#         for num in arr1:
+#             count[num] += 1
+#         result = []
+#         for num in arr2:
+#             result.extend([num] * count[num])
+#             count[num] = 0
+#         for i in range(1001):
+#             if count[i] > 0:
+#                 result.extend([i] * count[i])
+#         return result
