8eb09e7931649f9b765bae96328364befcd3a010
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-26 21:19:24 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1521,3 +1521,24 @@
 #             if count[i] > 0:
 #                 result.extend([i] * count[i])
 #         return result
+
+
+#############leetcode strictly increasing###################
+# class Solution:
+#     def validMountainArray(self, arr: List[int]) -> bool:
+#         strictly_increasing = False
+#         strictly_decreasing = False
+#
+#         for i in range(1, len(arr)):
+#             if arr[i] > arr[i - 1]:
+#                 if strictly_decreasing:
+#                     return False
+#                 strictly_increasing = True
+#             elif arr[i] < arr[i - 1]:
+#                 if not strictly_increasing:
+#                     return False
+#                 strictly_decreasing = True
+#             else:
+#                 return False
+#
+#         return True if strictly_increasing and strictly_decreasing else False
