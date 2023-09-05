5969dae984d66f5c985086843e9c70006aa82652
hrshlmeht <harshalmehta1998@gmail.com>
2023-09-05 16:52:29 -0400

    Leetcode

--- main.py
+++ main.py
@@ -1568,3 +1568,19 @@
 #             return (m1 + m2) / 2
 #         else:
 #             return sorted_arr[n // 2]
+
+
+#########leetcode wiggle subsequecne########
+# class Solution:
+#     def wiggleMaxLength(self, nums: List[int]) -> int:
+#
+#         inc = 1
+#         dec = 1
+#
+#         for i in range(1, len(nums)):
+#             if nums[i] > nums[i - 1]:
+#                 inc = dec + 1
+#             elif nums[i] < nums[i - 1]:
+#                 dec = inc + 1
+#
+#         return max(inc, dec)
