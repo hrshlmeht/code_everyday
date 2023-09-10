606afdaf99d668e647c0e064c1192276ef1febea
hrshlmeht <harshalmehta1998@gmail.com>
2023-09-10 17:58:39 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1607,7 +1607,23 @@
 #         return ans
 
 
-
+## 3 sum closest######
+# class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+#         closet = float('inf')
+#         nums.sort()
+#         for i in range(len(nums) - 2):
+#             l, r = i + 1, len(nums) - 1
+#             while l < r:
+#                 sum3 = nums[i] + nums[l] + nums[r]
+#                 print(sum3)
+#                 if sum3 < target:
+#                     l += 1
+#                 else:
+#                     r -=1
+#                 if abs(sum3 - target) < abs(closet - target):
+#                     closet = sum3
+#         return closet
 
 
 
