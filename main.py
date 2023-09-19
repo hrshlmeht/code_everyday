25401eb8590df1693663c50495bc797df0d5663b
hrshlmeht <harshalmehta1998@gmail.com>
2023-09-18 21:49:31 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1654,6 +1654,8 @@
 
 
 ###buying two chocolates##############
+
+
 # class Solution:
 #     def buyChoco(self, prices: List[int], money: int) -> int:
 #
@@ -1666,3 +1668,22 @@
 #                 return money
 #
 
+##rotating the array
+# class Solution:
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         new_lis = matrix.copy()
+#         matrix.clear()
+#
+#         for _ in range(len(new_lis)):
+#             matrix.append([])
+#         for i in range(len(new_lis)):
+#             for j in range(len(new_lis[0])):
+#                 # print(l[j][0])
+#                 div_ = j % len(new_lis[0])
+#                 matrix[j].insert(0, new_lis[i][div_])
+#
+#
+#         return(matrix)
