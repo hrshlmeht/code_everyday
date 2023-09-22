908f09602764943519d982ec1914e9a709f545e5
hrshlmeht <harshalmehta1998@gmail.com>
2023-09-21 20:18:25 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1738,3 +1738,24 @@
 # #         return max(ans1 , ans2)
 #
 #
+
+
+#######minimun sum index of 2 lists######
+# class Solution:
+#     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+#         res = len(list1) + len(list2)
+#         ans = {}
+#         x = []
+#         for word in list1:
+#             if word == " ":
+#                 continue
+#             if word in list2:
+#                 ln = list1.index(word) + list2.index(word)
+#                 res = min(res, ln)
+#                 ans[word] = ln
+#
+#         for key, val in ans.items():
+#             if val == res:
+#                 x.append(key)
+#
+#         return x
