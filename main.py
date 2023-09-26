5d7b77555497bdbc297b42764c3f30692a358c32
hrshlmeht <harshalmehta1998@gmail.com>
2023-09-25 20:26:37 -0400

    Leetcode problem solved

--- main.py
+++ main.py
@@ -1816,3 +1816,18 @@
 # #                 ans_final.append([nums1[index_list_1.index(i)], nums2[index_list_2.index(i)]])
 #
 # #         print(ans_final)
+
+## merge the arrays in a single one
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         tot = len(nums1)-1
+#         for i in range(len(nums2)-1,-1,-1):
+#             while m>0 and nums2[i]< nums1[m-1]:
+#                 nums1[tot] = nums1[m-1]
+#                 tot-=1
+#                 m-=1
+#             nums1[tot] = nums2[i]
+#             tot-=1
