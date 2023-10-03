ac5fddcf51f2c2c04cf4b0dbffd82673c676a8eb
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-02 23:00:43 -0400

    Interview 150- Leetcode

--- main.py
+++ main.py
@@ -1849,3 +1849,13 @@
 #
 #         return j
 #         print(nums)
+
+######remove duplicates in the array
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         j = 1
+#         for i in range(1, len(nums)):
+#             if nums[i] != nums[i - 1]:
+#                 nums[j] = nums[i]
+#                 j += 1
+#         return j
