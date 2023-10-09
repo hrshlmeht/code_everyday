e2cf86be4107b84646e922c1b2424c0dfca7951a
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-09 17:09:41 -0400

    Interview 150

--- main.py
+++ main.py
@@ -30,3 +30,18 @@
 #
 #         return j
 #         print(nums)
+
+
+#########removing duplicates from a sorted string######
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         if len(nums) <= 2:
+#             return len(nums)
+#
+#         currentIndex = 2
+#         for i in range(2, len(nums)):
+#             if nums[i] != nums[currentIndex - 2]:
+#                 nums[currentIndex] = nums[i]
+#                 currentIndex += 1
+#
+#         return currentIndex
