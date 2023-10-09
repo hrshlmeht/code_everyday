89da27b2e1ad2726226dd6a1bb05f01be73d7250
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-09 17:05:38 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1885,3 +1885,16 @@
 #
 #
 #
+###########remove duplicates from a sorted array##########
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         if len(nums) <= 2:
+#             return len(nums)
+#
+#         currentIndex = 2
+#         for i in range(2, len(nums)):
+#             if nums[i] != nums[currentIndex - 2]:
+#                 nums[currentIndex] = nums[i]
+#                 currentIndex += 1
+#
+#         return currentIndex
