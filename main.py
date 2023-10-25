fa7f1b731ffb2d43d91bb0c767c5dacb98864b18
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-24 21:35:24 -0400

    Basic background done

--- main.py
+++ main.py
@@ -1965,3 +1965,16 @@
 #                 ans.append(c + n)
 #
 #         return ans
+
+
+########running the encoded string#######
+# class Solution:
+#     def decompressRLElist(self, nums: List[int]) -> List[int]:
+#         result = []
+#         for i in range(0, len(nums), 2):
+#             freq = nums[i]
+#             val = nums[i + 1]
+#             result.extend([val] * freq)
+#
+#         print(result)
+#         return result
