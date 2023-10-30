1e8c9cf7bca0f48d9d051fece13a330bf518f3c3
hrshlmeht <harshalmehta1998@gmail.com>
2023-10-30 01:31:29 -0400

    Leetcode done

--- main.py
+++ main.py
@@ -1994,3 +1994,22 @@
 #                 closest_num = i
 #
 #         return closest_num
+
+
+################jump game 2######leetode medium
+#
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         jumps = 0
+#         next = 0
+#         current = 0
+#         i = 0
+#         n = len(nums)
+#         for i in range(0, n - 1):
+#             next = max(next, i + nums[i])
+#
+#             if current == i:
+#                 jumps += 1
+#                 current = next
+#
+#         return jumps
