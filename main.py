91a62588dec58a73d05f413f249928cba9487eb6
hrshlmeht <harshalmehta1998@gmail.com>
2024-10-04 22:23:33 -0400

    Back to leetcode

--- main.py
+++ main.py
@@ -2013,3 +2013,16 @@
 #                 current = next
 #
 #         return jumps
+
+##################buy and sell crypto#######
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         result = 0
+#
+#         lowest_price = prices[0]
+#         for price in prices:
+#             if price < lowest_price:
+#                 lowest_price = price
+#             result = max(result, price - lowest_price)
+#             print(result)
+#         return result
