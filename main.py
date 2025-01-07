479e69bc0614f673b989bd176868594489a8b3ec
hrshlmeht <harshalmehta1998@gmail.com>
2025-01-06 21:56:53 -0500

    Zig zag pattern leetcode

--- main.py
+++ main.py
@@ -2084,6 +2084,24 @@
 
 
 
+#################zigzag pattern string and matrix conversion paypal asked##@#
+#
+# class Solution:
+#     def convert(self, s: str, numRows: int) -> str:
+#         if numRows == 1 or numRows >= len(s):
+#             return s
+#
+#         rows = [''] * min(numRows, len(s))
+#         curRow, goingDown = 0, False
+#
+#         for c in s:
+#             rows[curRow] += c
+#             if curRow == 0 or curRow == numRows - 1:
+#                 goingDown = not goingDown
+#
+#             curRow += 1 if goingDown else -1
+#
+#         return ''.join(rows)
 
 
 
