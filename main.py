b3b89e7d2021b8241fd206ecc18b2ed26ac22baf
hrshlmeht <harshalmehta1998@gmail.com>
2025-01-07 21:29:23 -0500

    count prefix and suffix

--- main.py
+++ main.py
@@ -2105,3 +2105,16 @@
 
 
 
+#########count prefix and suffix####################
+# class Solution:
+#     def countPrefixSuffixPairs(self, words: List[str]) -> int:
+#
+#         count = 0
+#
+#         for i in range(len(words)):
+#             for j in range(i + 1, len(words)):
+#                 if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+#                     count += 1
+#
+#         return count
+
