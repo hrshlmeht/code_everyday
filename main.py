511a42475b098b350232a4eb0ad7d3b6e2c44037
hrshlmeht <harshalmehta1998@gmail.com>
2025-01-08 18:35:19 -0500

    Next Jump, leetcode 150

--- main.py
+++ main.py
@@ -2118,3 +2118,22 @@
 #
 #         return count
 
+
+#############ne4xt jump, leetcode 150####
+
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#
+#         max_reach = 0
+#
+#         for i in range(len(nums)):
+#             if i > max_reach:
+#                 return False
+#
+#             max_reach = max(max_reach, i + nums[i])
+#
+#             if max_reach >= len(nums) - 1:
+#                 return True
+#
+#         return False
