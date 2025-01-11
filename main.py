1e8a4ab5fe0736a7e003f706348ced361631754e
hrshlmeht <harshalmehta1998@gmail.com>
2025-01-11 15:06:22 -0500

    K palindromic - Leetcode daily

--- main.py
+++ main.py
@@ -2137,3 +2137,17 @@
 #                 return True
 #
 #         return False
+
+
+############construct K palindromic strings = Leetcode daily question############
+# class Solution:
+#     def canConstruct(self, s: str, k: int) -> bool:
+#         if len(s) < k:
+#             return False
+#         hashmap = Counter(s)
+#         count = 0
+#         for key, value in hashmap.items():
+#             if value % 2 == 1:
+#                 count += 1
+#         return count <= k
+#
