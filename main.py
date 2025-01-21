4881c92d59d3cc37722df3db0b3e0b1c8abaed9c
hrshlmeht <harshalmehta1998@gmail.com>
2025-01-20 22:24:26 -0500

    Leetcode daily - grid game

--- main.py
+++ main.py
@@ -2151,3 +2151,17 @@
 #                 count += 1
 #         return count <= k
 #
+#############grid game ## leetcode daily###
+
+# class Solution:
+#     def gridGame(self, grid: List[List[int]]) -> int:
+#         min_result = float('inf')
+#         row1_sum = sum(grid[0])
+#         row2_sum = 0
+#
+#         for i in range(len(grid[0])):
+#             row1_sum -= grid[0][i]
+#             min_result = min(min_result, max(row1_sum, row2_sum))
+#             row2_sum += grid[1][i]
+#
+#         return min_result
