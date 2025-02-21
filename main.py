6ff07e610d30a2d7ad555a216a937f27c5f353fd
hrshlmeht <harshalmehta1998@gmail.com>
2025-02-20 22:29:55 -0500

    Leetcode - Permutations

--- main.py
+++ main.py
@@ -2226,3 +2226,30 @@
 #                     num += 1
 #                 left += 1
 #         return matrix
+
+
+
+#####################permutations#######
+# class Solution:
+#     def permute(self, l: List[int]) -> List[List[int]]:
+#         def dfs(path, used, res):
+#             if len(path) == len(l):
+#                 res.append(
+#                     path[:])  # note [:] make a deep copy since otherwise we'd be append the same list over and over
+#                 return
+#
+#             for i, letter in enumerate(l):
+#                 # skip used letters
+#                 if used[i]:
+#                     continue
+#                 # add letter to permutation, mark letter as used
+#                 path.append(letter)
+#                 used[i] = True
+#                 dfs(path, used, res)
+#                 # remove letter from permutation, mark letter as unused
+#                 path.pop()
+#                 used[i] = False
+#
+#         res = []
+#         dfs([], [False] * len(l), res)
+#         return res
