9bf3242ba5295d83dd1b0fc5bb60fd56f1e344fa
RuchitaD <ruchitadhariaa@gmail.com>
2025-09-23 20:24:35 -0400

    Leetcode problem of the day

--- main.py
+++ main.py
@@ -2294,3 +2294,37 @@
 #             elif matrix[row][col] == target:
 #                 return True
 #         return False
+
+
+#leetcode problem of the day 
+
+#converting fration to decimal and handling the recurring decimal as well 
+
+# class Solution:
+#     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+#         if numerator == 0:
+#             return '0'
+        
+#         ans=[]
+
+#         if numerator < 0 and denominator > 0 or numerator > 0 and denominator < 0:
+#             ans.append('-')
+#         n,d = abs(numerator),abs(denominator)
+        
+#         ans.append(str(n//d))
+#         remainder = n % d
+#         if remainder==0:
+#             return ''.join(ans)
+#         ans.append('.')
+#         dict1={}
+#         while remainder != 0 and remainder not in dict1:
+#             dict1[remainder]=len(ans)
+#             remainder *= 10
+#             ans += str(remainder // d)
+#             remainder %= d
+#         if remainder in dict1:
+#             ans.insert(dict1[remainder],'(')
+#             ans.append(')')
+#             return ''.join(ans)
+
+#         return ''.join(ans)
