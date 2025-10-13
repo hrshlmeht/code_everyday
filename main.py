5e5270e18a268ce59609c2e0ce4f43c3b76237be
Harshal Mehta <62265727+hrshlmeht@users.noreply.github.com>
2025-10-12 22:29:53 -0400

    Leetcode Problem of the day

--- main.py
+++ main.py
@@ -2401,3 +2401,19 @@
 #                     longest = sub
 
 #         return longest
+
+
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         buy_price = prices[0]
+#         profit = 0
+
+#         for p in prices[1:]:
+#             if buy_price > p:
+#                 buy_price = p
+            
+#             profit = max(profit, p - buy_price)
+        
+#         return profit
