063ab1bf1292720d9abd84c44de7e6e8be2eb209
hrshl_meht <harshalmehta1998@gmail.com>
2025-12-29 21:25:40 -0500

    Neetcode hashmaps and array problem of the day

--- main.py
+++ main.py
@@ -2351,41 +2351,71 @@
 #         return count
 
 
-class MyHashSet:
+# class MyHashSet:
 
-    def __init__(self):
-        self.size = 10000
-        self.table = [None] * self.size
+#     def __init__(self):
+#         self.size = 10000
+#         self.table = [None] * self.size
     
-    def calculate_hash_value(self, key):
-        return key % self.size
+#     def calculate_hash_value(self, key):
+#         return key % self.size
 
-    def add(self, key: int) -> None:
-        hv = self.calculate_hash_value(key)
+#     def add(self, key: int) -> None:
+#         hv = self.calculate_hash_value(key)
         
-        if self.table[hv] == None:
-            self.table[hv] = [key]
-        else:
-            self.table[hv].append(key)
+#         if self.table[hv] == None:
+#             self.table[hv] = [key]
+#         else:
+#             self.table[hv].append(key)
 
-    def remove(self, key: int) -> None:
-        hv = self.calculate_hash_value(key)
+#     def remove(self, key: int) -> None:
+#         hv = self.calculate_hash_value(key)
         
-        if self.table[hv] != None:
-            while key in self.table[hv]: 
-                self.table[hv].remove(key)
+#         if self.table[hv] != None:
+#             while key in self.table[hv]: 
+#                 self.table[hv].remove(key)
 
-    def contains(self, key: int) -> bool:
-        hv = self.calculate_hash_value(key)
+#     def contains(self, key: int) -> bool:
+#         hv = self.calculate_hash_value(key)
         
-        if self.table[hv] != None:
-            return key in self.table[hv]
-        return False
+#         if self.table[hv] != None:
+#             return key in self.table[hv]
+#         return False
                 
 
 
-Your MyHashSet object will be instantiated and called as such:
-obj = MyHashSet()
-obj.add(key)
-obj.remove(key)
-param_3 = obj.contains(key)
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+
+# class MyHashSet:
+
+#     def __init__(self):
+
+#         self.data =[]
+        
+
+#     def add(self, key: int) -> None:
+#         if key not in self.data:
+#             self.data.append(key)
+
+
+        
+
+#     def remove(self, key: int) -> None:
+#         if key in self.data:
+#             self.data.remove(key)
+        
+
+#     def contains(self, key: int) -> bool:
+#         return key in self.data
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
