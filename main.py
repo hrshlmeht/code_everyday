fba2d06d14d39422539cb3f91875d12a9fdc9a3b
hrshl_meht <harshalmehta1998@gmail.com>
2025-12-30 21:06:56 -0500

    Leetcode problem of the data  -Sorting without using any of the inbuilt function

--- main.py
+++ main.py
@@ -2419,3 +2419,22 @@
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+
+
+# class Solution:
+#     def sortArray(self, nums: List[int]) -> List[int]:
+#         def counting_sort():
+#             count = defaultdict(int)
+#             minVal, maxVal = min(nums), max(nums)
+#             for val in nums:
+#                 count[val] += 1
+
+#             index = 0
+#             for val in range(minVal, maxVal + 1):
+#                 while count[val] > 0:
+#                     nums[index] = val
+#                     index += 1
+#                     count[val] -= 1
+
+#         counting_sort()
+#         return nums
