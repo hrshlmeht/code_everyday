b2dcbf25ea68a84da669a2ae6d5cdb96fe39a5cb
hrshl_meht <harshalmehta1998@gmail.com>
2026-03-25 18:54:48 -0400

    Leetcode problem of theday

--- main.py
+++ main.py
@@ -2448,3 +2448,37 @@
 #         for n in nums:
 #             ans.append(n)
 #         return ans
+
+
+# class Solution:
+#     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+#         m, n = len(grid), len(grid[0])
+        
+#         total = sum(sum(row) for row in grid)
+        
+#         # If total sum is odd, can't split equally
+#         if total % 2 != 0:
+#             return False
+        
+#         target = total // 2
+        
+#         # Try horizontal cuts
+#         curr = 0
+#         for i in range(m - 1):  # ensure bottom part is non-empty
+#             curr += sum(grid[i])
+#             if curr == target:
+#                 return True
+        
+#         # Try vertical cuts
+#         col_sums = [0] * n
+#         for j in range(n):
+#             for i in range(m):
+#                 col_sums[j] += grid[i][j]
+        
+#         curr = 0
+#         for j in range(n - 1):  # ensure right part is non-empty
+#             curr += col_sums[j]
+#             if curr == target:
+#                 return True
+        
+#         return False
